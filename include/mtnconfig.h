2a45fead374fa9532cd8d1a8eca8abd6a476941c
IAmMoltony <koronavirusnyj@gmail.com>
2023-10-25 15:23:41 +0600

    Add library files

---
+++ include/mtnconfig.h
@@ -0,0 +1,37 @@
+/**
+ * @file mtnconfig.h
+ * @brief Moltony's config library
+ * @author moltony
+*/
+
+#ifndef MTNCONFIG_H_
+#define MTNCONFIG_H_ 1
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/**
+ * @brief Initialize config
+ * @param configPath where config is located
+*/
+void mtnconfigInit(const char *configPath);
+
+/**
+ * @brief Get value from config with the given key as a string
+ * @param key entry key
+*/
+const char *mtnconfigGet(const char *key);
+
+/**
+ * @brief Get value from config with the given key as an integer
+ * @param key entry key
+*/
+int mtnconfigGetInt(const char *key);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
